1104f5fa4870bed2247d362bd6f7d10f971533bf
TheGieler <giel@raumzeitmedia.de>
2021-03-30 00:51:43 +0200

    Add notify event

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Product } from '../products';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -11,6 +12,9 @@ export class ProductAlertsComponent implements OnInit {
 
   @Input()
   product: Product;
+
+  @Output()
+  notify  = new EventEmitter();
 
   constructor() { }
 
