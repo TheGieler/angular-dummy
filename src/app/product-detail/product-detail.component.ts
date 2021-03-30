6d4f3bc1c52c90b466015aa55ae0bba7492b0edb
TheGieler <giel@raumzeitmedia.de>
2021-03-30 19:47:22 +0200

    Find product with provided id in route

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -20,6 +20,9 @@ export class ProductDetailComponent implements OnInit {
     // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
