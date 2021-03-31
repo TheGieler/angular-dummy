de2d1b3713854084497b50a2c80a6ba04967da4b
TheGieler <giel@raumzeitmedia.de>
2021-03-31 21:00:15 +0200

    Route CartComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'products/:productId', component: ProductDetailComponent },
+  {path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
