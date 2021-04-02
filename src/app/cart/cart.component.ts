9ee488ab69c9aaee902b154d1afe1ebc783fb09d
TheGieler <giel@raumzeitmedia.de>
2021-04-02 20:28:58 +0200

    Relocate wrong import

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cart',
