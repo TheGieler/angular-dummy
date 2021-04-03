9f3ec6d2f57342c887430462a50559b5dff144f3
TheGieler <giel@raumzeitmedia.de>
2021-04-04 00:24:55 +0200

    Prettier

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,31 +6,26 @@ import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-
   items = this.cartService.getItems();
 
   checkoutForm = this.formBuilder.group({
     name: '',
-    address: ''
+    address: '',
   });
 
   constructor(
     private cartService: CartService,
-    private formBuilder: FormBuilder,
-  ) {
+    private formBuilder: FormBuilder
+  ) {}
 
-   }
-
-   onSubmit(): void{
-     this.items = this.cartService.clearCart();
-     console.warn('Your order has been submitted', this.checkoutForm.value);
-     this.checkoutForm.reset();
-   }
-
-  ngOnInit(): void {
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
+  ngOnInit(): void {}
 }
