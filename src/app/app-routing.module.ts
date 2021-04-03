bad9b20fd5ae8a8b2a9644f7fbd1fc904c2622f5
TheGieler <giel@raumzeitmedia.de>
2021-04-03 19:55:56 +0200

    Add shipping routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from "./shipping/shipping.component";
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'products/:productId', component: ProductDetailComponent },
-  {path: 'cart', component: CartComponent }
+  {path: 'cart', component: CartComponent },
+  {path: 'shipping', component: ShippingComponent }
 ];
 
 @NgModule({
